175ef87baf932113a9fc20e7eb2518fb1f868ab0
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-30 18:31:44 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero></Hero>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
